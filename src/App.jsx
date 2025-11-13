7503cff7f3b24e66b24c6cc3559d52d44bcb234e
Hadid Hardiansyah Saputra <OUT2272@fifgroup.co.id>
2025-11-13 14:20:02 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+    return (
+        <main>
+            <div>
+                <h1>Welcome To 3D Portfolio</h1>
+            </div>
+        </main>
+    )
+}
+export default App
