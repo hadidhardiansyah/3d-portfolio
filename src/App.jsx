184d1dd7dca978164806ffcb1cc034ec11153a6d
Hadid Hardiansyah Saputra <OUT2272@fifgroup.co.id>
2025-11-23 21:21:45 +0700

    Create tech stack section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Hero from "./sections/Hero.jsx";
 import ShowcaseSection from "./sections/ShowcaseSection.jsx";
 import NavBar from "./components/NavBar.jsx";
 import ExperienceSection from "./sections/ExperienceSection.jsx";
+import TechStack from "./sections/TechStack.jsx";
 
 const App = () => {
     return (
@@ -10,6 +11,7 @@ const App = () => {
             <Hero />
             <ShowcaseSection />
             <ExperienceSection />
+            <TechStack />
         </>
     )
 }
